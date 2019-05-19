1294b129a8b453703e7e6066f95bbaeaf4128526
Timur <creedence38@gmail.com>
2019-05-19 16:16:50 +0300

    Test x
    Тестовые задания.

---
+++ Test 3/Test 3/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Test_3.Models;
+
+namespace Test_3.Controllers
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiController
+    {
+        public Product[] products = new Product[]
+        {
+            new Product {Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+            new Product {Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product {Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        };
+     
+
+
+        [HttpGet]
+        [Route("getProduct/{id}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("getCurrency")]
+        public async Task<IHttpActionResult> GetCurrency()
+        {
+            string accesskey = "cf6927fe016675b34a838c8208f98144";
+            string currency = "RUB";
+            string host = "apilayer.net";
+
+            string uri = $"http://{host}/api/live?access_key={accesskey}&currencies={currency}";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.GetAsync(uri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                var currencyModel = JsonConvert.DeserializeObject<CurrencyModel>(responseString);
+                return Json(currencyModel.quotes.USDRUB);
+            }
+
+        }
+    }
+}
---
+++ Test 3/Test 3/Models/CurrencyModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Test_3.Models
+{
+    public class Quotes
+    {
+        public double USDRUB { get; set; }
+    }
+
+    public class CurrencyModel
+    {
+        public bool success { get; set; }
+        public string terms { get; set; }
+        public string privacy { get; set; }
+        public int timestamp { get; set; }
+        public string source { get; set; }
+        public Quotes quotes { get; set; }
+    }
+}
